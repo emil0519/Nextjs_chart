cb25c96e53230258f3b3286423c114e9aad90ffd
Emil <emillau@nine-yi.com>
2024-04-14 13:20:50 +0800

    core: remove page.module.css & replace with MUI

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
-import { Header } from './component/header';
-import styles from './page.module.css'
-
+import { Box } from "@mui/material";
+import { Header } from "./component/header";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <Header />
-    </main>
-  )
+    <Box
+      component="main"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: "center",
+        minHeight: "100vh",
+      }}
+    >
+        <Header />
+    </Box>
+  );
 }
